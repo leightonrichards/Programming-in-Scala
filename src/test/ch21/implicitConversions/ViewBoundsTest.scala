38f3897bc8e1db8eb0a36fe76e897677fabe9328
Leighton Richards <leightonrichards@gmail.com>
2012-11-22 07:06:26 +0000

    add implicit ordering test

---
+++ src/test/ch21/implicitConversions/ViewBoundsTest.scala
@@ -0,0 +1,12 @@
+package ch21.implicitConversions
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: leighton
+ * Date: 22/11/12
+ * Time: 06:54
+ * To change this template use File | Settings | File Templates.
+ */
+class ViewBoundsTest {
+
+}
